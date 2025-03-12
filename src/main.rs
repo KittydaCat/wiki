a68f84f39a93a0d47887000326696b190b9fb4db
McCahey Fuerst <mccahey.fuerst@gmail.com>
2025-03-12 10:45:23 -0700

    save before nuke

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,9 @@
 // - Add support for different <> </> types
 
 use std::fs::{self, File};
-
 use serde_json::Value;
 use colored::Colorize;
-
-#[derive(Debug)]
-enum Markup<'a> {
-    DoubleBracket(&'a str),
-    DoubleBrace(&'a str),
-    Text(&'a str),
-}
+use either::Either;
 
 type Text<'a> = Vec<Span<'a>>;
 
@@ -34,7 +27,7 @@ enum SpanType<'a> {
 #[derive(Debug)]
 struct Template<'a> {
     name: &'a str,
-    options: Vec<&'a str>,
+    options: Vec<(Either<&'a str, u32>, &'a str)>,
 }
 
 #[derive(Debug)]
@@ -42,18 +35,26 @@ struct ArticleLink<'a> {
     article_name: &'a str,
 }
 
+#[derive(Debug)]
+struct Tag<'a> {
+    name: &'a str,
+    options: Vec<(Either<&'a str, u32>, &'a str)>,
+    inner_text: &'a str,
+}
+
+#[derive(Clone, Copy)]
 enum ParserState {
     Bracket,
     Brace,
     Angle,
-    Appostrophe(u32),
+    Apostrophe(u32),
     CBracket,
     CBrace,
     CAngle,
     None,
 }
 
-fn parse2<'a>(text: &'a str) -> Vec<Span<'a>> {
+fn parse(text: &str) -> Vec<Span> {
 
     let mut chars = text.char_indices();
 
@@ -63,7 +64,7 @@ fn parse2<'a>(text: &'a str) -> Vec<Span<'a>> {
 
     let mut inprogress: Vec<(SpanType, char)> = Vec::new();
 
-    while let Some((pos, char)) = chars.next() {
+    while let Some((mut pos, char)) = chars.next() {
 
         match (char, parser_state) {
             
@@ -71,11 +72,51 @@ fn parse2<'a>(text: &'a str) -> Vec<Span<'a>> {
 
             ('[', ParserState::None) => parser_state = ParserState::Bracket,
 
-            ('\'', ParserState::None) => parser_state = ParserState::Appostrophe(0),
+            ('\'', ParserState::None) => parser_state = ParserState::Apostrophe(0),
 
-            ('\'', ParserState::Appostrophe(x)) => parser_state = ParserState::Appostrophe(x + 1),
+            ('\'', ParserState::Apostrophe(x)) => parser_state = ParserState::Apostrophe(x + 1),
 
-           
+            // ('<', _) =>
+
+            ('{', ParserState::Brace) => {
+                //start parsing a template
+
+                // get the first arg
+                let ending_pos;
+
+                loop {
+
+                    let (y, x) = chars.next().expect("TODO");
+
+                    if (x == '|' || x =='}') {
+                        ending_pos = y;
+                        break;
+                    }
+
+                }
+
+                let template_name = dbg!(&text[pos+1..ending_pos]);
+
+                pos = ending_pos + 1;
+
+                loop {
+                    let ending_pos;
+
+                    loop {
+
+                        let (y, x) = chars.next().expect("TODO");
+
+                        if (x == '|' || x =='}') {
+                            ending_pos = y;
+                            break;
+                        }
+
+                    }
+                }
+
+                todo!();
+
+            }
 
             (_, ParserState::Brace | ParserState::Bracket) => parser_state = ParserState::None,        
 
@@ -89,157 +130,38 @@ fn parse2<'a>(text: &'a str) -> Vec<Span<'a>> {
 
 }
 
-fn parse(mut text: &str) -> Vec<Markup> {
-
-    let mut iter = text.char_indices();
-
-    let mut tokens = Vec::new();
-
-    let mut state = ParserState::None;
-
-    while let Some((pos, char)) = iter.next() {
-
-        // dbg!(pos,char);
-
-        state = match (char, state) {
-
-            ('[', ParserState::None) => {ParserState::Bracket},
-            ('[', ParserState::Bracket) => {
-
-                let (before, after) = text.split_at(pos-1);
-
-                tokens.push(Markup::Text(before));
-
-                iter = after.char_indices();
-
-                // to clear the two {{
-                iter.next();
-                iter.next();
-
-                let mut indent = 2;
-
-                let mut pos = 0;
-
-                // TODO make this work with unmatched single paras
-                while indent != 0 {
-
-                    let (x, char) = iter.next()
-                        .expect("Should not end with open brace");
-
-                    pos = x;
-
-                    match char {
-
-                        '[' => {indent += 1;}
-                        ']' => {indent -= 1;}
-
-                        _ => {}
-                    }
-
-                }
-
-                let (inside, remaining) = after.split_at(pos+1);
-
-                tokens.push(Markup::DoubleBracket(inside));
-
-                text = remaining;
-
-                iter = remaining.char_indices();
-
-                ParserState::None
-
-            },
-            ('{', ParserState::None) => {ParserState::Brace},
-            ('{', ParserState::Brace) => {
-
-                let (before, after) = text.split_at(pos-1);
-
-                tokens.push(Markup::Text(before));
-
-                iter = after.char_indices();
-
-                // to clear the two {{
-                iter.next();
-                iter.next();
-
-                let mut indent = 2;
-
-                let mut pos = 0;
-
-                // TODO make this work with unmatched single paras
-                while indent != 0 {
-
-                    let (x, char) = iter.next()
-                        .expect("Should not end with open brace");
-
-                    pos = x;
-
-                    match char {
-
-                        '{' => {indent += 1;}
-                        '}' => {indent -= 1;}
-
-                        _ => {}
-                    }
-
-                }
-
-                let (inside, remaining) = after.split_at(pos+1);
-
-                tokens.push(Markup::DoubleBrace(inside));
-
-                text = remaining;
-
-                iter = remaining.char_indices();
-
-                ParserState::None
-
-            },
-
-            (_, ParserState::Bracket | ParserState::Brace) => ParserState::None,
-            (_, ParserState::None) => ParserState::None,
-            _ => todo!(),
-        }
-
-    }
-
-    tokens.push(Markup::Text(text));
-
-    tokens
-
-}
-
 fn main() {
 
     // let mess = reqwest::blocking::get("https://api.wikimedia.org/core/v1/wikipedia/en/page/Cat_(Unix)").unwrap();
 
     // let body: Value = mess.json().unwrap();
-    let file = fs::read("rawtext.txt").unwrap();
 
-    let body: Value = serde_json::from_slice(&file).unwrap();
-    
-    let raw_page = body.as_object().unwrap().get("source").unwrap().as_str().unwrap();
+    let raw_page = fs::read_to_string("rawtext.txt").unwrap();
 
-    // println!("{}", raw_page);
+    // let body: Value = serde_json::from_slice(&file).unwrap();
+    //
+    // let raw_page = body.as_object().unwrap().get("source").unwrap().as_str().unwrap();
+
+    dbg!(parse(&raw_page));
 
     // dbg!(parse("Helle[[wor{ld [[ feak] as}da] asa]] dada"));
     // dbg!(parse("Helle{{wor[[ld [[ feak] asda] asa]] }}dada"));
 
     // dbg!(parse("Helle[[wor{ld [[ feak] as}da] asa]] dada Helle{{wor[[ld [[ feak] asda] asa]] }}dada"));
 
-    for x in parse(raw_page) {
-        match x {
-            Markup::DoubleBracket(txt) => {
-                print!("{}", txt
-                    .strip_prefix("[[").unwrap()
-                    .strip_suffix("]]").unwrap()
-                    .blue()
-                )
-            }
-            Markup::DoubleBrace(_) => { print!("!") }
-            Markup::Text(txt) => { print!("{}", txt) }
-        }
-    }
+    // for x in parse(raw_page) {
+    //     match x {
+    //         Markup::DoubleBracket(txt) => {
+    //             print!("{}", txt
+    //                 .strip_prefix("[[").unwrap()
+    //                 .strip_suffix("]]").unwrap()
+    //                 .blue()
+    //             )
+    //         }
+    //         Markup::DoubleBrace(_) => { print!("!") }
+    //         Markup::Text(txt) => { print!("{}", txt) }
+    //     }
+    // }
 
     // println!("{}", page);
 }
